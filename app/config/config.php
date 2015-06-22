bc3cf76397755bb8ff1a12efe0c4fb34b74bc5d0
Michael <hungnv038@gmail.com>
2015-06-22 17:22:29 +0700

    ddd

--- app/config/config.php
+++ app/config/config.php
@@ -19,12 +19,8 @@ return new \Phalcon\Config(array(
         'viewsDir'       => APP_PATH . '/app/views/',
         'pluginsDir'     => APP_PATH . '/app/plugins/',
         'libraryDir'     => APP_PATH . '/app/library/',
-<<<<<<< HEAD
         'cacheDir'       => APP_PATH . '/app/storage/cache/',
-=======
-        'cacheDir'       => APP_PATH . '/app/cache/',
         'debugDir'       => APP_PATH . '/app/vendor/PDW/',
->>>>>>> 0cf219134a410494dbcbbc782d5f4e18dd7dd6ba
         'baseUri'        => '/',
     )
 ));
