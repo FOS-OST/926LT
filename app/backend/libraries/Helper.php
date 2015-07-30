e83d784180bf06455932788e814743bbcad810a2
hieutrieu <mrhieutrieu@gmail.com>
2015-07-30 17:38:39 +0700

    aaaaa

--- app/backend/libraries/Helper.php
+++ app/backend/libraries/Helper.php
@@ -108,11 +108,11 @@ class Helper {
         switch($type) {
             case 'SINGLE':
                 $stickerIcon = $answer['correct'] ? '<i class="fa fa-dot-circle-o"></i> ':'<i class="fa fa-circle-o"></i> ';
-                $stickerIcon .= $answer['answer'];
+                $stickerIcon = "<div class='col-xs-1'>{$stickerIcon}</div><div class='col-xs-11'>{$answer['answer']}</div>";
                 break;
             case 'MULTI':
                 $stickerIcon = $answer['correct'] ? '<i class="fa fa-check-square-o"></i> ':'<i class="fa fa-square-o"></i> ';
-                $stickerIcon .= $answer['answer'];
+                $stickerIcon = "<div class='col-xs-1'>{$stickerIcon}</div><div class='col-xs-11'>{$answer['answer']}</div>";
                 break;
             case 'FREE_TEXT':
                 $stickerIcon = "<div style='margin-bottom:5px;'>{$answer['answer']} = <span style='border:1px solid #808080;padding:1px 10px;'>{$answer['correct']}</span></div>";
